626cceb8bc0902f8afb5f198f56b3c5478c3784b
Mariia Sechenova <sechenova.m.a@yandex.ru>
2022-11-14 18:41:28 +0700

    Initial Commit

---
+++ src/Auto.java
@@ -0,0 +1,17 @@
+public class Auto {
+    public static Auto.Name Name;
+
+
+    enum Name {
+        VITZ, YARIS, OPA, CAMRY
+    }
+
+    int power;
+    int weight;
+    public static Auto.Type Type;
+
+    enum Type {
+        SEDAN, HATCHBACK, PICKUP, VAN, TRUCK, MICRO, CABRIOLET
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Auto MyCar = new Auto();
+        MyCar.Name = Auto.Name.YARIS;
+        MyCar.power = 1000;
+        MyCar.weight = 2000;
+        MyCar.Type = Auto.Type.HATCHBACK;
+
+        Engine MyEngine = new Engine();
+        SteeringWheel MySteeringWheel = new SteeringWheel();
+
+        System.out.println(MyCar.Name + " is " + MyCar.Type + " which weights " + MyCar.weight + " and has power " + MyCar.power + " did the following: ");
+        MyEngine.startEngine();
+        MySteeringWheel.turnLeft();
+        MySteeringWheel.turnRight();
+        MyEngine.stopEngine();
+
     }
 }
